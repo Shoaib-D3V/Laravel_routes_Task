cf2b4b1ddeeb69b2ae51583bae07afe06a9dfca8
Shoaib Qureshi <shoaib@d3vtech.com>
2024-11-12 16:24:33 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+
 Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'v1'], function() {
-    // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
-    // Add ONE line to assign 5 resource routes to TaskController
     Route::apiResource('tasks', TaskController::class);
 });
+
