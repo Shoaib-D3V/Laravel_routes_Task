9fd829b1292bc4f9ad62c749223a57617f15883f
Shoaib Qureshi <shoaib@d3vtech.com>
2024-11-13 19:05:07 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,26 +19,6 @@ use App\Http\Controllers\Admin\StatsController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-namespace App\Http\Controllers;
-
-use App\Models\User;
-use Illuminate\Http\Request;
-
-class UserController extends Controller
-{
-    public function show($name)
-    {
-        $user = User::where('name', $name)->first();
-
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
-        }
-
-        return response()->json($user);
-    }
-}
-
-
 
 
 // Task 1: point the main "/" URL to the HomeController method "index"
